337a6df27591128ad5cae54cbeaf7f9c48823594
chr-alexander-bahrdt <christian.a.bahrdt@campus.tu-berlin.de>
2019-09-25 20:06:27 +0200

    Implement dummy driver.

--- driver/simpledriver.go
+++ driver/simpledriver.go
@@ -17,6 +17,7 @@ import (
 	"image/jpeg"
 	"image/png"
 	"os"
+	"strconv"
 	"time"
 
 	dsModels "github.com/edgexfoundry/device-sdk-go/pkg/models"
@@ -72,12 +73,14 @@ func (s *SimpleDriver) Initialize(lc logger.LoggingClient, asyncCh chan<- *dsMod
 
 // HandleReadCommands triggers a protocol Read operation for the specified device.
 func (s *SimpleDriver) HandleReadCommands(deviceName string, protocols map[string]contract.ProtocolProperties, reqs []dsModels.CommandRequest) (res []*dsModels.CommandValue, err error) {
+	fmt.Fprintf(os.Stderr, "error: %s\n", "hello you...")
 
 	if len(reqs) != 1 {
 		err = fmt.Errorf("SimpleDriver.HandleReadCommands; too many command requests; only one supported")
 		return
 	}
 	s.lc.Debug(fmt.Sprintf("SimpleDriver.HandleReadCommands: protocols: %v resource: %v attributes: %v", protocols, reqs[0].DeviceResourceName, reqs[0].Attributes))
+	fmt.Fprintf(os.Stderr, "error: %s\n", "hello you...")
 
 	res = make([]*dsModels.CommandValue, 1)
 	now := time.Now().UnixNano() / int64(time.Millisecond)
@@ -94,7 +97,22 @@ func (s *SimpleDriver) HandleReadCommands(deviceName string, protocols map[strin
 		}
 		cvb, _ := dsModels.NewBinaryValue(reqs[0].DeviceResourceName, now, buf.Bytes())
 		res[0] = cvb
+	} else if reqs[0].DeviceResourceName == "output" {
+		cvs := dsModels.NewStringValue(reqs[0].DeviceResourceName, now, "this is your results. it's nothing.")
+		fmt.Fprintf(os.Stderr, "error: %s\n", "hello you results...")
+		s.lc.Info("Got commando message to device resource" +  reqs[0].DeviceResourceName)
+		res[0] = cvs
+	} else if reqs[0].DeviceResourceName == "status" {
+		cvs := dsModels.NewStringValue(reqs[0].DeviceResourceName, now, "current status.")
+		fmt.Fprintf(os.Stderr, "error: %s\n", "hello you... status")
+		s.lc.Info("Got commando message to device resource" +  reqs[0].DeviceResourceName)
+		res[0] = cvs
+	} else {
+		s.lc.Info("I could not handle anything:" +  reqs[0].DeviceResourceName)
+		fmt.Fprintf(os.Stderr, "error: %s\n", "hello you... anything")
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 	}
+
 	return
 }
 
@@ -104,24 +122,39 @@ func (s *SimpleDriver) HandleReadCommands(deviceName string, protocols map[strin
 // command.
 func (s *SimpleDriver) HandleWriteCommands(deviceName string, protocols map[string]contract.ProtocolProperties, reqs []dsModels.CommandRequest,
 	params []*dsModels.CommandValue) error {
-
-	if len(reqs) != 1 {
-		err := fmt.Errorf("SimpleDriver.HandleWriteCommands; too many command requests; only one supported")
-		return err
-	}
-	if len(params) != 1 {
-		err := fmt.Errorf("SimpleDriver.HandleWriteCommands; the number of parameter is not correct; only one supported")
-		return err
-	}
-
-	s.lc.Debug(fmt.Sprintf("SimpleDriver.HandleWriteCommands: protocols: %v, resource: %v, parameters: %v", protocols, reqs[0].DeviceResourceName, params))
-	var err error
-	if s.switchButton, err = params[0].BoolValue(); err != nil {
-		err := fmt.Errorf("SimpleDriver.HandleWriteCommands; the data type of parameter should be Boolean, parameter: %s", params[0].String())
-		return err
+	drName := reqs[0].DeviceResourceName
+	cmdMessage := "Got command: " + drName + " with params: "
+	s.lc.Info(cmdMessage)
+	for index, param := range params {
+		paramMessage := "{"
+		paramMessage += "Param No. " + strconv.Itoa(index) + ":"
+		paramMessage += param.DeviceResourceName + ":"
+		paramMessage += "::"
+		stringValue, _ := param.StringValue()
+		paramMessage += stringValue
+		paramMessage += "}"
+		s.lc.Info(paramMessage)
 	}
 
 	return nil
+
+	//if len(reqs) != 1 {
+	//	err := fmt.Errorf("SimpleDriver.HandleWriteCommands; too many command requests; only one supported")
+	//	return err
+	//}
+	//if len(params) != 1 {
+	//	err := fmt.Errorf("SimpleDriver.HandleWriteCommands; the number of parameter is not correct; only one supported")
+	//	return err
+	//}
+	//
+	//s.lc.Debug(fmt.Sprintf("SimpleDriver.HandleWriteCommands: protocols: %v, resource: %v, parameters: %v", protocols, reqs[0].DeviceResourceName, params))
+	//var err error
+	//if s.switchButton, err = params[0].BoolValue(); err != nil {
+	//	err := fmt.Errorf("SimpleDriver.HandleWriteCommands; the data type of parameter should be Boolean, parameter: %s", params[0].String())
+	//	return err
+	//}
+	//
+	//return nil
 }
 
 // Stop the protocol-specific DS code to shutdown gracefully, or
